2f89cb7a1dbd39a1258df55a7fcc60a021d8f4c1
hjzheld <dusen7774@naver.com>
2023-09-06 21:06:21 +0900

    DB에 들어갈 todo 내용추가

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    title = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_complete = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
