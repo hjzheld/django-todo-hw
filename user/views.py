393c6937eadabc6a6808c74ee1e43d7859f5e348
hjzheld <dusen7774@naver.com>
2023-09-06 21:09:09 +0900

    user 앱 로그인, 회원가입 추가

--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from user.models import User
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        User.objects.create_user(username=username, password=password)
+        return redirect("/todo/")
+    elif request.method == "GET":
+        return render(request, "signup.html")
+        
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            auth_login(request, user)
+            return redirect("/todo/")
+        else:
+            return HttpResponse("Invaild auth", status=401)
+    elif request.method == "GET":
+        return render(request, "login.html")
+    else :
+        return HttpResponse("Invaild auth", status=401)
+    
