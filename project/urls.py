662089b3c3a11c3dee7af6bf1eba886f61243206
hjzheld <dusen7774@naver.com>
2023-09-07 11:51:51 +0900

    user앱/todo앱 추가

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.list),
     path('user/',include("user.urls")),
     path('todo/',include("todo.urls")),
 ]
